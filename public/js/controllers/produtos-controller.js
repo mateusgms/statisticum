9ee27d781cc34abd86f4bd3d2880c2b17f09b157
Mateusgms <mateusgms@icloud.com>
2017-02-03 00:33:26 -0200

    GET sandbox

--- public/js/controllers/produtos-controller.js
+++ public/js/controllers/produtos-controller.js
@@ -6,7 +6,7 @@ angular.module("dashboardApp")
             method:'GET',  url:'http://sandbox.buscape.com.br/service/findProductList/lomadee/2b306b414d622f4c6137733d/BR/?sourceId=35716948&keyword=pneu&format=json'
         })       
 		.success(function(data, status, headers, config){
-            $scope.product.product = data.product.product;
+            $scope.product = data.product;
         })
         .error(function(data, status, headers, config){
             console.log(status);
